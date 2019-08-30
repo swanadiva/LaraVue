60bebe6096e616e42690462c77aa00258086bd1d
Swana Diva Borneos <diva937@gmail.com>
2019-08-31 02:41:41 +0700

    Display all questions

---
+++ resources/views/questions/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">All Questions</div>
+
+                <div class="card-body">
+                    @foreach($questions as $q)
+                        <div class="media">
+                            <div class="media-body">
+                                <h3 class="mt-0">
+                                    {{ $q->title }}
+                                </h3>
+                                {{ str_limit($q->body, 250) }}
+                            </div>
+                        </div>
+                        <hr>
+                    @endforeach
+
+                    <div class="mx-auto">
+                        {{ $questions->links() }}
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
